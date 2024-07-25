6564a0550ed67808b41131830c19c4c2b13a2e1d
horo053 <yulfi555@gmail.com>
2024-07-25 23:00:11 +0300

    update lesson 4.2 in PythonDeveloperS

--- PythonDeveloperS/Level_4/lesson_2.py
+++ PythonDeveloperS/Level_4/lesson_2.py
@@ -4,7 +4,5 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
 # TODO здесь ваш код
-from district.central_street.house1 import room1 as cr1, room2 as cr2
-from district.soviet_street.house1 import room1 as sr1, room2 as sr2
-print(f"На Центральной улице живут: {', '.join(cr1.folks)}, {', '.join(cr2.folks)}")
-print(f"На Советской улице живут: {', '.join(sr1.folks)}, {', '.join(sr2.folks)}")
+from house import room_1, room_2
+print(f"На Центральной улице живут: {', '.join(room_1.folks)}, {', '.join(room_2.folks)}")
