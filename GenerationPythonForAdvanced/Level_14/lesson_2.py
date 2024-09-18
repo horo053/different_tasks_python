0441be0ff0e64985bec2cd251d7f73a6ab0013d0
horo <horo053@mail.ru>
2024-09-18 20:22:59 +0300

    update 14.2 in GenerationPythonForAdvanced

--- GenerationPythonForAdvanced/Level_14/lesson_2.py
+++ GenerationPythonForAdvanced/Level_14/lesson_2.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 # 1. Напишите программу, которая рисует пунктирную линию
 turtle.showturtle()
@@ -79,7 +80,71 @@ turtle.shape('turtle')
 n = 38
 
 for i in range(n):
-  turtle.forward(i*2)
-  turtle.right(22)
-  turtle.stamp()
+    turtle.forward(i*2)
+    turtle.right(22)
+    turtle.stamp()
 
+
+# 7. Напишите программу, которая рисует узор в соответствии с образцом.
+turtle.showturtle()
+
+pensize = 1
+pencolor = ['red', 'green', 'yellow', 'orange', 'blue']
+
+for i in range(40):
+    turtle.forward(i*3)
+    turtle.left(60)
+    turtle.pencolor(random.choice(pencolor))
+
+    pensize += 0.2
+    turtle.pensize(pensize)
+
+
+# 8. Напишите программу, которая рисует звезду, показанную на рисунке. Такую звезду можно создать из двух треугольников.
+# Однако их невозможно нарисовать непрерывной линией, поэтому перо нужно будет поднять для перехода ко второму треугольнику.
+turtle.showturtle()
+
+turtle.goto(-50, 100)
+turtle.goto(-100, 0)
+turtle.goto(0, 0)
+
+turtle.penup()
+turtle.goto(0, 70)
+
+turtle.pendown()
+turtle.goto(-100, 70)
+turtle.goto(-50, -30)
+turtle.goto(0, 70)
+
+
+# ИЛИ
+for i in range(3):
+  turtle.forward(100)
+  turtle.left(120)
+
+turtle.penup()
+turtle.goto(0,60)
+turtle.pendown()
+
+for i in range(3):
+  turtle.forward(100)
+  turtle.right(120)
+
+
+# 9. Напишите программу, которая рисует изображение в соответствии с образцом.
+turtle.showturtle()
+turtle.speed(1)
+turtle.pencolor('green')
+
+for i in range(-200, 201, 40):
+    if i == 0:
+        continue
+    else:
+        turtle.goto(i, -200)
+        turtle.dot(10, 'blue')
+        turtle.goto(0,0)
+
+turtle.dot(10, 'red')
+
+
+# 10. Напишите программу, которая рисует изображение символа олимпиады в соответствии с образцом.
